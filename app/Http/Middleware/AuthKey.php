aeb379e2651fdd9cc323cd2c9c561d74ee58f3a6
artur <artur@vis-desing.com>
2022-04-27 11:26:36 +0200

    init

--- app/Http/Middleware/AuthKey.php
+++ app/Http/Middleware/AuthKey.php
@@ -2,10 +2,8 @@
 
 namespace App\Http\Middleware;
 
-use App\Providers\RouteServiceProvider;
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class AuthKey
 {
@@ -13,7 +11,7 @@ class AuthKey
     {
         $token = $request->header('token');
 
-        if ($token != 'rqweqwxa))13300') {
+        if ($token != 'rq1weqwxa))13300') {
             return response()->json([
                'message' => 'the token is wrong'
             ], 401);
